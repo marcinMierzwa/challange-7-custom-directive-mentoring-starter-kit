80d3f40533d9437a99e59342b3f38ce7e2bc955d
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2024-11-27 23:39:56 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,24 @@ import { Component, signal, WritableSignal } from '@angular/core';
 import { ResizeDirective } from './resize.directive';
 import { ValidateColorDirective } from './validate-color.directive';
 import { ErrorMessageDirective } from './error-message.directive';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ResizeDirective, ValidateColorDirective, ErrorMessageDirective],
+  imports: [FormsModule, NgIf, ResizeDirective, ValidateColorDirective, ErrorMessageDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'mentoring-program-starter-kit';
-  isColorValid = signal(false);
+  // isColorValid = signal(false);
 
-  handleColorValidation(isColorValid: boolean) {
-    this.isColorValid.set(isColorValid);
+  // handleColorValidation(isColorValid: boolean) {
+  //   this.isColorValid.set(isColorValid);
     
-  }
+  // }
   
 
 
--- src/app/validate-color.directive.ts
+++ src/app/validate-color.directive.ts
@@ -1,43 +1,85 @@
 import {
   Directive,
-  EventEmitter,
-  HostListener,
   inject,
-  Output,
   Renderer2,
-  signal,
 } from '@angular/core';
 import { RectangleService } from './rectangle.service';
+import { FormControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
   selector: '[appValidateColor]',
   standalone: true,
+  providers: [
+    { 
+    provide: NG_VALIDATORS, useExisting:ValidateColorDirective, multi: true
+    }],
 })
-export class ValidateColorDirective {
-  @Output() emitIsColorValid = new EventEmitter();
+export class ValidateColorDirective implements Validator {
+
   renderer: Renderer2 = inject(Renderer2);
   rectangleService: RectangleService = inject(RectangleService);
-  isColorValid = signal(false);
 
-  @HostListener('input', ['$event'])
-  validateColor(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    const color = input.value;
-    const rectangle = document.querySelector('.rectangle');
-    const rgb = this.rectangleService.hexToRgb(color);
-    const { r, g, b } = rgb;
 
-    if (r > g + b) {
-      this.renderer.setStyle(rectangle, 'background-color', color);
-      this.isColorValid.set(false);
+  validate(control: FormControl): ValidationErrors | null {
+    const color = control.value;
+  
+    if (color != null) {
+      const rectangle = document.querySelector('.rectangle');
+      const rgb = this.rectangleService.hexToRgb(color);
+      const { r, g, b } = rgb;
+  
+      if (r > g + b) {
+        this.renderer.setStyle(rectangle, 'background-color', color);
+        return null;
+      } else {
+        this.renderer.setStyle(rectangle, 'backgroundColor', '#0000ff');
+        return { invalidColorError: true }; // Dodano zwrot błędu
+      }
     } else {
-      this.renderer.setStyle(
-        document.querySelector('.rectangle'),
-        'backgroundColor',
-        '#0000ff'
-      );
-      this.isColorValid.set(true);
+      return {
+        invalidColorError: true
+      };
     }
-    this.emitIsColorValid.emit(this.isColorValid());
   }
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+// @Output() emitIsColorValid = new EventEmitter();
+// renderer: Renderer2 = inject(Renderer2);
+// rectangleService: RectangleService = inject(RectangleService);
+// isColorValid = signal(false);
+
+// @HostListener('input', ['$event'])
+// validateColor(event: Event): void {
+//   const input = event.target as HTMLInputElement;
+//   const color = input.value;
+//   const rectangle = document.querySelector('.rectangle');
+//   const rgb = this.rectangleService.hexToRgb(color);
+//   const { r, g, b } = rgb;
+
+//   if (r > g + b) {
+//     this.renderer.setStyle(rectangle, 'background-color', color);
+//     this.isColorValid.set(false);
+//   } else {
+//     this.renderer.setStyle(
+//       document.querySelector('.rectangle'),
+//       'backgroundColor',
+//       '#0000ff'
+//     );
+//     this.isColorValid.set(true);
+//   }
+//   this.emitIsColorValid.emit(this.isColorValid());
+// }
+
